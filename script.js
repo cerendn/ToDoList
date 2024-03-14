3db082e6cbc04e2f6faec18e0c07503a48df8c4b
cerendn <cerendncer@outlook.com>
2024-03-14 18:59:57 +0300

    js1

---
+++ script.js
@@ -0,0 +1,4 @@
+const todoInput = document.querySelector(".todoInput");
+const todoButton = document.querySelector(".todoButton");
+const todoList = document.querySelector(".todoList");
+const filter = document.querySelector(".filter");
